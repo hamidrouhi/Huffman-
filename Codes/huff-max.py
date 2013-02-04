c586528d36b506a8922ed4490595e70683cb965b
revolt313 <hrouhi.kh@gmail.com>
2013-02-04 18:00:17 +0330

    update huff-max.py

--- Codes/huff-max.py
+++ Codes/huff-max.py
@@ -8,15 +8,15 @@ def heap_gen(L):
 	global tree,parent
 	tree=list(Data)
 	while len(tree)>1:
-		LCh=min(tree)
-		index=tree.index(LCh)
-		LChL=list(tree[index])
+		LeftChild=min(tree)
+		index=tree.index(LeftChild)
+		LeftChidindex=list(tree[index])
 		del tree[index]
-		RCh=min(tree)
-		index=tree.index(RCh)
-		RChL=list(RCh)
+		RightChild=min(tree)
+		index=tree.index(RightChild)
+		RightChildindex=list(RightChild)
 		del tree[index]
-		parent=(LChL[0]+RChL[0],LCh,RCh)
+		parent=(LeftChidindex[0]+RightChildindex[0],LeftChild,RightChild)
 		tree.append(parent)
 	return tree[0]
 
@@ -24,12 +24,12 @@ def codes(huffheap, hbeats = ''):
 	global probs,var_list_
 	if len(huffheap) == 2:
 		print huffheap[1], hbeats
-		Pi=int(data_for_get_avrg_len[huffheap[1]])
-		Ni=len(hbeats)
-		Nbar=float(Pi*Ni)
-		Vi=float(Pi*((Ni-Nbar)** 2 ))
+		Prob_i=int(data_for_get_avrg_len[huffheap[1]])
+		Num_i=len(hbeats)
+		Nbar=float(Prob_i*Num_i)
+		Var_i=float(Prob_i*((Num_i-Nbar)** 2 ))
 		probs.append(Nbar)
-		var_list_.append(Vi)
+		var_list_.append(Var_i)
 	else:
 		codes(huffheap[1], hbeats + '0')
 		codes(huffheap[2], hbeats + '1')
@@ -43,17 +43,17 @@ def variance_of_codes(var_list_):
 	return Z_var_
 
 def get_probs():
-	print "Symbolss : "
-	Symbs=int(raw_input("++>"))
-	for i in range(l,Symbs+1):
-		print "Symbol %d :"%i
-		Char=str(raw_input("symbol  ==> : "))
+	print "Symbolls : "
+	Symbolls=int(raw_input("++>"))
+	for i in range(Symbolls):
+		print "Symboll %d :"%i
+		Char=str(raw_input("symboll  ==> : "))
 		Num=float(raw_input("probability ==> : "))
 		print
 		data=Num,Char
 		Data.append((tuple(data)))
 		data_for_get_avrg_len[Char]=Num
-		if i==Symbs:break
+		if i==Symbolls:break
 	huffheap = heap_gen(Data)
 	codes(huffheap)
 
